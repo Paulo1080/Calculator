5e51374a88c2b160b697590563da0057e2dae798
Paulo Moraes <pmoraes1080@gmail.com>
2021-12-21 16:02:55 -0300

    dev...

--- js/Controller/CalculatorController.js
+++ js/Controller/CalculatorController.js
@@ -1,8 +1,9 @@
 class CalculatorController {
     constructor() {
 
-        this.dateEl = document.querySelector('.date');
-        this.hourEl = document.querySelector('.hour');
+        this._dateEl = document.querySelector('.date');
+        this._hourEl = document.querySelector('.hour');
+        this._displayEl = document.querySelector('.expression');
         this._expressionList = ['0'];
         this.initialize();
         this.initAddEventsButtons();
@@ -15,11 +16,79 @@ class CalculatorController {
         }, 1000)
     }
 
+    inverse(){
+        if(this.ferifyOperator(this.returnLast())){
+            this._expressionList.pop();
+        }
+        if(this.returnLast()=='0'){
+            return;
+        }
+        this._expressionList[this._expressionList.length-1] = (1/this.returnLast()).toString();
+        this.attDisplay();
+        
+    }
+
     attDate() {
         let date = new Date();
 
-        this.dateEl.innerHTML = date.toLocaleDateString('pt-BR');
-        this.hourEl.innerHTML = date.toLocaleTimeString('pt-BR');
+        this._dateEl.innerHTML = date.toLocaleDateString('pt-BR');
+        this._hourEl.innerHTML = date.toLocaleTimeString('pt-BR');
+    }
+
+    attDisplay(){
+        this._displayEl.innerHTML = this._expressionList.join('');
+        this._displayEl.scrollBy(100,0);
+    }
+
+    clear(){
+        this._expressionList = ['0'];
+        this.attDisplay();
+    }
+
+    eraser(){
+        this._expressionList[this._expressionList.length-1] = this.returnLast().slice(0,-1);
+        if(this.returnLast() == ''){
+            if(this._expressionList.length == 1){
+                this._expressionList = ['0']
+            }else{
+                this._expressionList.pop();
+            }
+        }
+        this.attDisplay();
+    }
+
+    returnLast(){
+        return this._expressionList[this._expressionList.length-1];
+    }
+
+    ferifyOperator(val){
+
+        return ['×','÷','+','-'].indexOf(val)>-1;
+    }
+
+    addExpressionValues(val){
+        if(this.ferifyOperator(val)){
+            if(this.ferifyOperator(this.returnLast())){
+                this._expressionList[this._expressionList.length-1] = val;
+            }else{
+                this._expressionList.push(val);
+            }            
+        }else{
+            if(this.ferifyOperator(this.returnLast())){
+                this._expressionList.push(val);
+            }else{
+                if(this.returnLast() == '0' && val.toString() != '.'){
+                   this._expressionList[this._expressionList.length-1] = ''; 
+                }
+                if(this.returnLast().indexOf('.')>-1 && val.toString() == '.'){
+                    return;
+                }
+                this._expressionList[this._expressionList.length-1] += val.toString();
+            }
+            
+        }
+        console.log(this._expressionList);
+        this.attDisplay();
     }
 
     initAddEventsButtons() {
@@ -31,12 +100,15 @@ class CalculatorController {
 
                 switch (value) {
                     case 'AC':
+                        this.clear();
                         break;
                     case 'backspace':
+                        this.eraser();
                         break;
                     case '=':
                         break;
                     case '1/x':
+                        this.inverse();
                         break;
                     case '+':
                     case '-':
@@ -53,6 +125,7 @@ class CalculatorController {
                     case '9':
                     case '0':
                     case '.':
+                        this.addExpressionValues(value);
                         break;
                 }
             })
